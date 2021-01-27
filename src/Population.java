812323def32cef0d886f291518f423846de089be
Jiongxiao <2609728289@qq.com>
2021-01-27 19:12:46 +0800

    Initial commit

---
+++ src/Population.java
@@ -0,0 +1,8 @@
+public class Population {
+    public static void main(String[] args){
+        double pp = 7100000000.0;
+        long cp = 1400000000;
+        System.out.printf("2019年世界人口总数约：%.0f\n" ,pp);
+        System.out.println("2019年中国人口总数约：" +cp);
+    }
+}
